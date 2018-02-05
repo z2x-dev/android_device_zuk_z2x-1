47295ca684fce13dbe6a49df2b5a11ebc2be51c7
Caneray <punisher_caner@windowslive.com>
2018-02-05 18:16:16 +0000

    z2x: Bypass to TWRP App

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,1 +1,8 @@
-add_lunch_combo omni_z2x-userdebug
+# Lunch Combo's
+for var in eng user userdebug; do
+  add_lunch_combo omni_z2x-$var
+done
+
+
+# TWRP Bypass Indicator
+$(cd bootable/recovery && git apply ../../device/zuk/z2x/twrp_app.patch)
